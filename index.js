28cac78fb8cbddf5c960a5dcc0fc02457ee99394
Baysangur Elbiev <Nepstor95@gmail.com>
2022-08-16 19:01:07 +0300

    js

---
+++ index.js
@@ -0,0 +1,58 @@
+// console.log('Hello,World!')
+// console.log(2+3)
+// console.log(10-2);
+// console.log(2*3);
+// console.log(10/5)
+// console.log(2**3)
+
+// console.log((2+3)+(10-2)+(2*3)+(10/5)+(2**3))
+
+// let b1 = 29
+// let b2 = 10
+// let b3 = 1994
+// let b4 = 27
+// let b5 = 16
+
+
+
+// b2 = 11
+// b3 = 1993
+// b4 = 25
+
+
+
+
+// let a1 = 'My name is Baysangur'
+// let a2 = 'My fullnmae Elbiev'
+// let a3 = 'My father name Bers'
+// let a4 = 'My mother name Liza'
+// let a5 = 'I am live Urus-Martan'
+// console.log(b1,b2,b3,b4,b5)
+// console.log(a1,a2,a3,a4,a5)
+// b2 = 11
+// b3 = 1993
+// b4 = 25
+
+// a2 = 'My fullname Bersanow'
+// a3 = 'My father Beckhan'
+
+// console.log(b2,b3,b4)
+// console.log(a2,a3)
+
+
+
+
+// let nums = [1,2,3,4,5, -10,2]
+// let numes = ["Adam","Ali"] 
+// console.log(nums)
+// console.log(numes)
+
+
+
+let array1 = ['Beckhan', 27 , 'Alihan', 24, 'Khadijat', 22, 'karina', 20,'Aishat', 19]
+console.log('Имя', array1[0], ',', 'Возраст', array1[1])
+console.log('Имя',array1[2],',','Возрост',array1[3])
+console.log('Имя',array1[4],',','Возрост',array1[5])
+console.log('Имя',array1[6],',','Возрост',array1[7])
+console.log('Имя',array1[8],',','Возрост',array1[9])
+
